c621857d7a724142ad83f3d376162082e7831132
Igor Amroas <igorduarteamoras@gmail.com>
2021-08-17 14:08:19 -0300

    Plugin improvement

--- src/middlewares/namePlugin.js
+++ src/middlewares/namePlugin.js
@@ -7,12 +7,10 @@ module.exports = {
       }
     })
     schema.pre("save", async function(next){
-      console.log('InitialLetter')
       let inis = '';
-      if(!this.username)return next()
-      // this.model.findByID({}) -> Acessar o modelo referenciado 
-      this.username.split(' ').forEach(name => {
-        if(names.length === 1){inis = name.substring(0,1); return;}
+      const {name: completeName} = await this.model('User').findById(this.user)
+      completeName.split(' ').forEach(name => {
+        if(completeName.length === 1){inis = name.substring(0,1); return;}
         inis += name.charAt(0)
       })
       this.initials = inis
